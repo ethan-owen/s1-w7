047ab89ad35c1d76263bcb16e61190ab792dcd36
Ethan Owen <ethanowen848@gmail.com>
2026-09-21 12:14:58 -0700

    wk 7 day 1

---
+++ Weight.java
@@ -0,0 +1,13 @@
+public class Weight {
+    private int pounds;
+    private int ounces;
+
+    public Weight(int ounces, int pounds) {
+        this.pounds = pounds;
+        this.ounces = ounces;
+    }
+
+    public Weight(int ounces) {
+        this.ounces = ounces;
+    }
+}
---
+++ WeightMain.java
@@ -0,0 +1,5 @@
+public class WeightMain {
+    public static void main(String[] args) {
+        
+    }
+}

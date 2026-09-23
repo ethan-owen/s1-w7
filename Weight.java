c7b8b74a97460eed029dce190dc314845c47855e
Ethan Owen <ethanowen848@gmail.com>
2026-09-23 12:16:01 -0700

    wk 7 day 2

--- Weight.java
+++ Weight.java
@@ -2,12 +2,40 @@ public class Weight {
     private int pounds;
     private int ounces;
 
-    public Weight(int ounces, int pounds) {
-        this.pounds = pounds;
-        this.ounces = ounces;
+    public Weight (int ounces, int pounds) {
+        if (ounces >=- 0) {
+            if(ounces<16) {
+                this.pounds = pounds;                    
+                this.ounces = ounces;
+            }
+        }
     }
 
     public Weight(int ounces) {
-        this.ounces = ounces;
+        this(ounces, 0);
+    }
+
+    public int totalOunces(){
+        return this.ounces + this.pounds * 16;
+    }
+
+    public boolean isHeavier(Weight other){
+        int totalW = this.totalOunces();
+        int otherW = other.totalOunces();
+        if (totalW > otherW){
+            return true;
+        } 
+        return false;
+    }
+
+    public Weight multiple(int k){
+            int newWeight = this.totalOunces() * k;
+            int p = newWeight/16;
+            int o = newWeight % 16;
+            return new Weight(o, p);
+    }
+
+    public void print(){
+        System.out.println(this.pounds + " pounds, " + this.ounces + " ounces.");
     }
 }
--- WeightMain.java
+++ WeightMain.java
@@ -1,5 +1,12 @@
 public class WeightMain {
     public static void main(String[] args) {
-        
+        Weight w1 = new Weight(12, 9);
+        Weight w2 = new Weight(7, 11);
+        w1.print();
+        System.out.println(w1.isHeavier(w2));
+        Weight w3 = w1.multiple(2);
+        System.out.println(w3.isHeavier(w2));
+        w1.print();
+        w3.print();
     }
 }
